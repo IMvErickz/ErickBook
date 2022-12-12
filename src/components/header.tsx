c17bb4851f87774fef211b5956f8c90ba806a230
IMvErickz <erickspy2003@gmail.com>
2022-12-12 17:04:15 -0300

    fixed: nav responsive

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import Logo from '../assets/tools-and-utensils.png'
 
 export function Header() {
     return (
-        <div className='flex flex-row items-start justify-start gap-x-64'>
+        <div className='flex flex-row items-start justify-start w-full'>
             <nav className="bg-background flex flex-row items-center justify-center">
                 <div className='flex flex-row items-start justify-start'>
                     <div className='flex flex-col items-start justify-start '>
@@ -11,8 +11,8 @@ export function Header() {
                 </div>    
                     
                 
-            </nav> <br />
-            <div className="flex flex-col items-center justify-center">
+            </nav> 
+            <div className="flex flex-col items-center justify-center w-full">
                         <h1 className="text-field text-6xl font-bold font-apple">Erick Santos</h1>
             </div>
         </div>
