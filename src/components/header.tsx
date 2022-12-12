99dffa644b1efde7c3973a34ef261984f090b306
IMvErickz <erickspy2003@gmail.com>
2022-12-12 17:16:29 -0300

    modified navbar

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,19 +2,15 @@ import Logo from '../assets/tools-and-utensils.png'
 
 export function Header() {
     return (
-        <div className='flex flex-row items-start justify-start w-full'>
+        <div className='flex flex-row items-center justify-center w-full'>
             <nav className="bg-background flex flex-row items-center justify-center">
                 <div className='flex flex-row items-start justify-start'>
-                    <div className='flex flex-col items-start justify-start '>
-                        <img src={Logo} alt="" className='w-20'/>
+                    <div className='flex flex-row items-center justify-center gap-x-4'>
+                        <img src={Logo} alt="" className='w-20' />
+                        <h1 className="text-field text-6xl font-bold font-apple">Erick Santos</h1>
                     </div>
                 </div>    
-                    
-                
             </nav> 
-            <div className="flex flex-col items-center justify-center w-full">
-                        <h1 className="text-field text-6xl font-bold font-apple">Erick Santos</h1>
-            </div>
         </div>
     )
 }
