e3f2cf549a46f3fde33cdd4cee468e8d52ba0753
IMvErickz <erickspy2003@gmail.com>
2022-12-30 20:59:29 -0300

    added a gradient in tittle home

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ export function Header() {
                 <div className='flex flex-row items-start justify-start'>
                     <div className='flex flex-row items-center justify-center gap-x-4'>
                         <img src={Logo} alt="" className='w-20' />
-                        <h1 className="text-field text-6xl font-bold font-apple">Erick Santos</h1>
+                        <h1 className="text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purplee via-greeen to-cyyan font-bold font-apple">Erick Santos</h1>
                     </div>
                 </div>    
             </nav> 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,7 +8,10 @@ module.exports = {
       colors: {
         background: '#202024',
         bgNav: '#4c1d95',
-        field: '#312e81'
+        field: '#312e81',
+        purplee: '#5609b4',
+        greeen: ' #118c01',
+        cyyan: ' #06d5cc'
       },
 
       fontFamily: {
